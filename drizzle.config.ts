73f9deccfafeb8c1b90abeeacb8ca2eb429e517f
Dane Stevens <dane@dsmedia.ca>
2025-11-05 13:04:01 -0500

    test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,5 @@ console.log(process.env.DATABASE_URL)
 export default defineConfig({
   dialect: 'mysql',
   schema: './drizzle/schema.ts',
-  dbCredentials: {
-    url: String(process.env.DATABASE_URL)
-  }
+  url: String(process.env.DATABASE_URL)
 })
