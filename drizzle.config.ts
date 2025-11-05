7526c9af637434153e7859e26bc534fca08fa0e0
Dane Stevens <dane@dsmedia.ca>
2025-11-05 12:12:31 -0500

    placeholder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   dialect: 'sqlite',
   schema: './drizzle/schema.ts',
-  url: process.env.DATABASE_URL
+  dbCredentials: {
+    url: process.env.DATABASE_URL
+  }
 })
