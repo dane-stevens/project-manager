5dfdd0e907e4d5123f206e90b7e0e9f871f6e757
Dane Stevens <dane@dsmedia.ca>
2025-11-06 12:43:55 -0500

    test

--- app/components/Forms.tsx
+++ app/components/Forms.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react"
+import { useFetchers } from "react-router"
 
 export function Field({
   type,
@@ -21,5 +22,8 @@ export function Field({
 }
 
 export function Submit({ children }: { children: ReactNode }) {
-  return <button type="submit" className="bg-zinc-200 text-zinc-900 px-4 py-2 rounded-lg disabled:text-red-600" > {children}</ button>
+  const fetchers = useFetchers()
+  const fetcher = fetchers?.[0]
+  const isLoading = ['loading', 'submitting'].includes(fetcher?.state)
+  return <button type="submit" className="bg-zinc-200 text-zinc-900 px-4 py-2 rounded-lg disabled:text-red-600" disabled={isLoading}>{children}</button>
 }
